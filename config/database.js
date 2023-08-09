d5df34f9e3883302a21a5d2a9d3373e20f0ffac5
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-08-09 15:37:29 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -15,11 +15,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-ch7sn7msi8uhth2tialg-a.singapore-postgres.render.com'),  
+      host: env('DATABASE_HOST', 'dpg-cj9lrvu3ttrc73e6p1cg-a.singapore-postgres.render.com'),  
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ecommerce_database_upjt'),
-      user: env('DATABASE_USERNAME', 'ecommerce_database_upjt_user'),
-      password: env('DATABASE_PASSWORD', 'uga0sCM9f9KVdSCjv2fpuET0VM8cvxYU'),
+      database: env('DATABASE_NAME', 'ecommerce_databse'),
+      user: env('DATABASE_USERNAME', 'ecommerce_databse_user'),
+      password: env('DATABASE_PASSWORD', 'BObg5t4zK8RDaeqA4INjuAcvKlkbFHcT'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
